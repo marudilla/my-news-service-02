014bf9774723fe40bd046807e473cfcf48ada8b3
Miguel <49211342+marudilla@users.noreply.github.com>
2024-02-16 11:12:39 +0000

    espacio

--- buildScripts/setEnv.sh
+++ buildScripts/setEnv.sh
@@ -4,7 +4,7 @@
 export MY_AZ_ACR_URL="miguelamp24acr.azurecr.io"
 export AZ_BASE_IMAGE_REPO="msr-1015-lean-original-recipe"
 export AZ_BASE_IMAGE_FIXES_TAG="Fixes_24-02-12"
-export AZ_BASE_IMAGE_TAG="${MY_AZ_ACR_URL}/${AZ_BASE_IMAGE_REPO}: ${AZ_BASE_IMAGE_FIXES_TAG}"
+export AZ_BASE_IMAGE_TAG="${MY_AZ_ACR_URL}/${AZ_BASE_IMAGE_REPO}:${AZ_BASE_IMAGE_FIXES_TAG}"
  
 export AZ_ACR_REPO_NAME="my-news-service-02"
 export OUR_SERVICE_TAG_BASE="${MY_AZ_ACR_URL}/${AZ_ACR_REPO_NAME}"
